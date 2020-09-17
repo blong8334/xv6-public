589389dcfbf65342a8bbb9512fb926a74d54d273
brianlong <blong8334@gmail.com>
2020-09-17 16:04:31 -0400

    Finished first implementation of uniq

--- uniq.c
+++ uniq.c
@@ -3,35 +3,78 @@
 #include "user.h"
 #include "fs.h"
 
-/*
-  How would you do this in node?
-    read the file.
-    split at the new lines.
-    set the first sentence to base.
-    compare the second sentence. 
-    move on. 
-*/
-
-typedef enum
+int loadFirstWord(char *file, char *cache)
 {
-  true,
-  false
-} bool;
-
-void cacheWord(int *wordIdx, char buf[], char wordCache[])
-{
-  int cacheIdx = 0;
-  char value;
-  while ((value = buf[*wordIdx]))
+  int fileIdx = 0;
+  while (1)
   {
-    *wordIdx += 1;
-    if (value == '\n')
+    char value = file[fileIdx];
+    if (value == '\0')
     {
-      wordCache[cacheIdx] = '\0';
-      return;
+      return fileIdx;
     }
-    wordCache[cacheIdx++] = value;
+    *cache = value;
+    cache += 1;
+    fileIdx += 1;
+    *cache = '\0';
+    if (value == '\n')
+      return fileIdx;
   }
+}
+
+void writeCacheToResults(char *cache, char *results)
+{
+  int length = strlen(results);
+  while (*cache != '\0')
+  {
+    results[length++] = *cache;
+    results[length] = '\0';
+    cache += 1;
+  }
+}
+
+void generateUniq(char *file, char *cache, char *results)
+{
+  results[0] = '\0';
+  int cacheIdx = 0, uniqCount = 1;
+  int fileIdx = loadFirstWord(file, cache);
+  file = &file[fileIdx];
+  while (*file != '\0')
+  {
+    if (cacheIdx >= strlen(cache) || *file != cache[cacheIdx])
+    {
+      writeCacheToResults(cache, results);
+      while (1)
+      {
+        cache[cacheIdx++] = *file;
+        cache[cacheIdx] = '\0';
+        if (*file == '\0')
+        {
+          break;
+        }
+        if (*file == '\n')
+        { // starting new word
+          uniqCount = 1;
+          file += 1;
+          cacheIdx = 0;
+          break;
+        }
+        file += 1;
+      }
+    }
+    else if (*file == '\n')
+    {
+      uniqCount += 1;
+      file += 1;
+      cacheIdx = 0;
+    }
+    else
+    {
+      cacheIdx += 1;
+      file += 1;
+    }
+  }
+  writeCacheToResults(cache, results);
 }
 
 int main(int argc, char *argv[])
@@ -39,13 +82,13 @@ int main(int argc, char *argv[])
   char *fileName = argv[1];
   int fd = open(fileName, 0);
   struct stat statBuf;
-  int status = fstat(fd, &statBuf);
+  fstat(fd, &statBuf);
   int fileSize = statBuf.size + 1;
-  char fileString[fileSize];
-  read(fd, fileString, fileSize);
-  char wordCache[fileSize];
-  int wordIndex = 0;
-  cacheWord(&wordIndex, fileString, wordCache);
-  cacheWord(&wordIndex, fileString, wordCache);
+  char file[fileSize];
+  file[fileSize - 1] = '\0';
+  read(fd, file, fileSize);
+  char results[fileSize];
+  char cache[fileSize];
+  generateUniq(file, cache, results);
   exit();
 }
